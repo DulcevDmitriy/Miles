87304a3d1b3ee49f95b8e7f8c8529843f085fc26
Dulcev Dmitriy <ddvvvvvv@yandex.ru>
2023-01-11 15:03:37 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price =10000;
+        int price = 10000;
 
-        int BonusMiles=price / 20;
-        System.out.println( "Бонусные мили:" + BonusMiles);
+        int BonusMiles = price / 20;
+        System.out.println("Бонусные мили:" + BonusMiles);
     }
 }
